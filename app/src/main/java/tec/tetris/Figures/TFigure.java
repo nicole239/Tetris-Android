4567aa652995ab059fb2f6615d8be2c14c8914d1
Nicole <nicole.carvajal.b@gmail.com>
2019-08-24 14:05:16 -0600

    T figure fixed and L Figure done

--- app/src/main/java/tec/tetris/Figures/TFigure.java
+++ app/src/main/java/tec/tetris/Figures/TFigure.java
@@ -20,16 +20,25 @@ public class TFigure extends AbstractFigure {
                 rotation++;
                 break;
             case 1:
-                coordenates[3][0] -= 2;
+                coordenates[2][0]--;
+                coordenates[2][1]++;
                 rotation++;
                 break;
             case 2:
+                coordenates[3][0]--;
+                coordenates[3][1]--;
+                rotation++;
+                break;
+            case 3:
                 coordenates[1][0]--;
                 coordenates[1][1]--;
 
-                coordenates[3][0]+=2;
-                rotation = 0;
+                coordenates[2][0]++;
+                coordenates[2][1]--;
 
+                coordenates[3][0]++;
+                coordenates[3][1]++;
+                rotation = 0;
         }
     }
 
@@ -37,19 +46,25 @@ public class TFigure extends AbstractFigure {
     public boolean collidesRotation(int[][] board) {
         switch(rotation){
             case 0:
-                if ( board [coordenates[1][0]++][coordenates[1][1]++] == Board.OCCUPIED_SPACE)
+                if ( board [coordenates[1][0]+1][coordenates[1][1]+1] == Board.OCCUPIED_SPACE)
                     return true;
                 break;
             case 1:
-                if ( board [coordenates[3][0]-=2][coordenates[3][1]] == Board.OCCUPIED_SPACE)
+                if ( board [coordenates[2][0]-1][coordenates[2][1]+1] == Board.OCCUPIED_SPACE)
                     return true;
                 break;
             case 2:
-                if ( board [coordenates[1][0]--][coordenates[1][1]--] == Board.OCCUPIED_SPACE &&
-                        board [coordenates[3][0]+=2][coordenates[3][1]] == Board.OCCUPIED_SPACE)
-
+                if ( board [coordenates[3][0]-1][coordenates[3][1]-1] == Board.OCCUPIED_SPACE)
                     return true;
                 break;
+
+            case 3:
+                if (    board [coordenates[1][0]-1][coordenates[1][1]-1] == Board.OCCUPIED_SPACE &&
+                        board [coordenates[2][0]+1][coordenates[2][1]-1] == Board.OCCUPIED_SPACE &&
+                        board [coordenates[3][0]+1][coordenates[3][1]+1] == Board.OCCUPIED_SPACE  )
+                    return true;
+                break;
+
         }
         return false;
     }
