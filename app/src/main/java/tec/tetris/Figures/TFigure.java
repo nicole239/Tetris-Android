a498c7f2f5641596fdb1890f26ed733dd83dee38
Nicole <nicole.carvajal.b@gmail.com>
2019-08-24 12:14:00 -0600

    Figure T class

--- app/src/main/java/tec/tetris/Figures/TFigure.java
+++ app/src/main/java/tec/tetris/Figures/TFigure.java
@@ -1,21 +1,56 @@
 package tec.tetris.Figures;
 
+import tec.tetris.Board;
+
 public class TFigure extends AbstractFigure {
 
     private int rotation;
 
     public TFigure(){
-        coordenates = new int[][] {{2,1},{1,2},{2,2},{3,2}};
+        coordenates = new int[][] {{2,2},{1,2},{2,1},{3,2}};
         rotation = 0;
     }
 
     @Override
     public void rotate() {
+        switch(rotation){
+            case 0:
+                coordenates[1][0]++;
+                coordenates[1][1]++;
+                rotation++;
+                break;
+            case 1:
+                coordenates[3][0] -= 2;
+                rotation++;
+                break;
+            case 2:
+                coordenates[1][0]--;
+                coordenates[1][1]--;
 
+                coordenates[3][0]+=2;
+                rotation = 0;
+
+        }
     }
 
     @Override
     public boolean collidesRotation(int[][] board) {
+        switch(rotation){
+            case 0:
+                if ( board [coordenates[1][0]++][coordenates[1][1]++] == Board.OCCUPIED_SPACE)
+                    return true;
+                break;
+            case 1:
+                if ( board [coordenates[3][0]-=2][coordenates[3][1]] == Board.OCCUPIED_SPACE)
+                    return true;
+                break;
+            case 2:
+                if ( board [coordenates[1][0]--][coordenates[1][1]--] == Board.OCCUPIED_SPACE &&
+                        board [coordenates[3][0]+=2][coordenates[3][1]] == Board.OCCUPIED_SPACE)
+
+                    return true;
+                break;
+        }
         return false;
     }
 }
